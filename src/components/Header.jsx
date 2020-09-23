61eb680537f2e4b278f62a3d561ae05c086cd7d1
liam027 <liam027@gmail.com>
2020-09-23 16:20:27 -0400

    Improve the header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 
 const Header = (props) => {
-  return(
-    <h1 style={{ textAlign: 'center' }}>{props.title}</h1>
+
+  const headerStyle = {
+    display: 'grid',
+    gridTemplateColumns: '1fr 8fr 1fr',
+    padding: '20px 0px',
+    backgroundColor: 'black'
+  }
+  const titleStyle = {
+    gridColumn: 2,
+    textAlign: 'center',
+    fontSize: '2em',
+    fontWeight: 'bold',
+    color: 'white',
+  }
+  const loginStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'middle',
+    padding: '10px 40px',
+    color: 'white',
+    backgroundColor: '#3568ff',
+    borderRadius: '10px',
+    cursor: 'pointer'
+  }
+
+  return (
+    <header style={headerStyle}>
+      <span style={titleStyle}>{props.title}</span>
+      <div style={loginStyle}>
+        <span >Login</span>
+      </div>
+    </header>
   )
 }
 
