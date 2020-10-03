a4eff5c68e2b536ca85e7c595ee6424207b1e1fc
liam027 <liam027@gmail.com>
2020-10-03 17:11:22 -0400

    Add cypress package

--- src/components/ItemCard.test.js
+++ src/components/ItemCard.test.js
@@ -1,5 +1,5 @@
 import ItemCard from './ItemCard'
-import prettyDOM from '@testing-library/dom'
+// import prettyDOM from '@testing-library/dom'
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
