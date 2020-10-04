1f2310fc9758a88a829948b509aa138600a8f5a9
liam027 <liam027@gmail.com>
2020-10-04 09:37:03 -0400

    Setup test database before each integration test
    Only a single user (root) with no products is available at the start of
    the tests

--- cypress/integration/grocery_app.spec.js
+++ cypress/integration/grocery_app.spec.js
@@ -1,4 +1,16 @@
 describe('Grocery app', function () {
+
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      username: 'root',
+      email: 'email@email.com',
+      password: 'password'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
   it('front page can be opened', function () {
     cy.visit('http://localhost:3000')
     cy.contains('Groceryio')
