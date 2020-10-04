bd19f866c48e17c8fabb5d3bf8a573f65b09d6f4
liam027 <liam027@gmail.com>
2020-10-04 09:27:52 -0400

    Add a new product integration test

--- cypress/integration/grocery_app.spec.js
+++ cypress/integration/grocery_app.spec.js
@@ -1,10 +1,10 @@
-describe('Grocery app', function() {
-  it('front page can be opened', function() {
+describe('Grocery app', function () {
+  it('front page can be opened', function () {
     cy.visit('http://localhost:3000')
     cy.contains('Groceryio')
   })
 
-  it('user can login', function() {
+  it('user can login', function () {
     cy.visit('http://localhost:3000')
     cy.contains('Login').click()
 
@@ -13,6 +13,24 @@ describe('Grocery app', function() {
     cy.get('#login-submit').click()
 
     cy.contains('Logout')
+  })
+
+  describe('when logged in', function () {
+    beforeEach(function () {
+      cy.visit('http://localhost:3000')
+      cy.contains('Login').click()
+
+      cy.get('#username').type('root')
+      cy.get('#password').type('password')
+      cy.get('#login-submit').click()
+    })
+
+    it('can add new product', function () {
+      cy.contains('Add Product').click()
+      cy.get('#new-product-input').type('unique product')
+      cy.contains('Save').click()
+      cy.contains('UNIQUE PRODUCT')
+    })
   })
 
 })
