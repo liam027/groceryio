6fbe13733112018fae064e7fba0c1ab932fa83a8
liam027 <liam027@gmail.com>
2020-10-29 09:54:00 -0400

    Access API for a specific users products

--- src/services/user.js
+++ src/services/user.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
 const baseUrl = '/api/users'
 
+const get = async (user_id) => {
+  const response = await axios.get(`${baseUrl}/${user_id}`)
+  return response.data
+}
+
 const create = async (newUser) => {
   const response = await axios.post(baseUrl, newUser)
   return response.data
 }
 
-export default { create }
+export default { get, create }
